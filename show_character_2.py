6b1e6d174bf0b613bed748b9a5e9194d35690334
hiroshi <hprek20160101@gmail.com>
2017-05-27 16:17:48 +0900

    コミット

--- show_character_2.py
+++ show_character_2.py
@@ -7,3 +7,30 @@ test_1
 test_2
 """
 print(test_str)
+
+test_str = 'python'
+test_str = test_str + '-'
+test_str = test_str + 'izm'
+
+print(test_str)
+
+test_str = 'python'
+test_str = test_str + '-'
+test_str = test_str + 'izm'
+
+print(test_str)
+
+test_str = '012'
+test_str += '345'
+test_str += '678'
+test_str += '9'
+
+print(test_str)
+
+test_str = '012' * 3
+
+print(test_str)
+
+test_integer = 100
+
+print(str(test_integer) + '円')
